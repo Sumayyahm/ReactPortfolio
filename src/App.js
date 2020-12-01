ea5a96ba527a7080c6c46147328d055a3439e6ee
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-12-01 16:12:43 -0600

    Created App.js file to put together all components

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Routes from "./Routes";
+import Footer from "./components/Footer";
+
+
+
+
+
+
+function App() {
+
+    return (
+        <div className="body">
+        <Routes />
+        <Footer />
+        </div>
+    )
+}
+
+export default App;
