92c7e02d305b708034f81fe7cf50ad790e73dc26
Sumayyah Mariam <sumayyahmariam019@gmail.com>
2020-12-01 16:10:18 -0600

    Created Project Component nested inside portfolio to display each project

---
+++ src/components/Project.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { Item } from 'semantic-ui-react'
+
+const style = {
+    fontFamily: 'Cambria',
+    color: 'dimgray'
+  }
+
+
+const Projects = (props) => (
+    <Item.Group>
+    <Item>
+    <br></br>
+      <Item.Image size='big' src={props.imageURL} />
+
+      <Item.Content>
+        <br></br>
+        <Item.Header style={style}>{props.name}</Item.Header>
+        <br></br><br></br>
+        <Item.Description style={style}>
+          <p>{props.description}</p>
+          <br></br>
+          <p><a href= {props.deployed} target="_blank">Deployed Link</a></p>
+          <br></br>
+          <p><a href={props.github} target="_blank">GitHub Link</a></p>
+        </Item.Description>
+      </Item.Content>
+    </Item>
+    </Item.Group>
+ 
+)
+
+export default Projects
